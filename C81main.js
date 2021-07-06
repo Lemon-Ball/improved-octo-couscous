e827a548f198319c961aedd10452d4004778dd95
Lemon-Ball <86389566+Lemon-Ball@users.noreply.github.com>
2021-07-05 21:04:16 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,30 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color=document.getElementById("color").value;
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 40, 0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mouse_down);
+function my_mouse_down(e){
+    color1=document.getElementById("color").value;
+    console.log(color1);
+
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    console.log("x = " + mouse_x + ", y = " + mouse_y);
+    circle(mouse_x, mouse_y);
+}
+function circle(mouse_x, mouse_y){
+    ctx.beginPath();
+ctx.strokeStyle=color1;
+ctx.lineWidth=2;
+ctx.arc(mouse_x, mouse_y, 40, 0,2*Math.PI);
+ctx.stroke();
+}
+function clear(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
